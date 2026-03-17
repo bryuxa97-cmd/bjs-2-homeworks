c044f3e25ff3152555f85d2e333f1ef2e59e4cfb
AndreyCope2 <i@cope2.ru>
2026-03-17 20:49:39 +0400

    Две задачи готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,35 +1,35 @@
 // Здание №1
 "use strict";
 
-function solveEquation(a, b, c) {
-  const discriminant = b ** 2 - 4 * a * c;
+function solveEquation(coefficientA, coefficientB, coefficientC) {
+  const discriminant = coefficientB ** 2 - 4 * coefficientA * coefficientC;
 
   if (discriminant < 0) {
     return [];
   }
 
   if (discriminant === 0) {
-    return [-b / (2 * a)];
+    return [-coefficientB / (2 * coefficientA)];
   }
 
   return [
-    (-b + Math.sqrt(discriminant)) / (2 * a),
-    (-b - Math.sqrt(discriminant)) / (2 * a),
+    (-coefficientB + Math.sqrt(discriminant)) / (2 * coefficientA),
+    (-coefficientB - Math.sqrt(discriminant)) / (2 * coefficientA),
   ];
 }
 
 // Задание №2
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const P = percent / 100 / 12;
-  const S = amount - contribution;
+  const monthlyInterestRate = percent / 100 / 12;
+  const loanBody = amount - contribution;
 
-  if (S <= 0) {
+  if (loanBody <= 0) {
     return 0;
   }
 
-  const payment = S * (P + P / (Math.pow(1 + P, countMonths) - 1));
-  const total = payment * countMonths;
+  const monthlyPayment = loanBody * (monthlyInterestRate + monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, countMonths) - 1));
+  const total = monthlyPayment * countMonths;
 
   return Math.round(total * 100) / 100;
 }
