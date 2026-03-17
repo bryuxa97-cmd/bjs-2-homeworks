803de20ccefdc032cc6fcff73fe4a0add04c4473
AndreyCope2 <i@cope2.ru>
2026-03-17 20:21:42 +0400

    Две задачи готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,4 @@
 // Здание №1
-// "use strict";
-
-// function solveEquation(a, b, c) {
-//   const discriminant = b ** 2 - 4 * a * c;
-
-//   if (discriminant < 0) {
-//     return [];
-//   }
-
-//   if (discriminant === 0) {
-//     return [-b / (2 * a)];
-//   }
-
-//   return [
-//     (-b + Math.sqrt(discriminant)) / (2 * a),
-//     (-b - Math.sqrt(discriminant)) / (2 * a),
-//   ];
-// }
-
-// function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-// }
-
-// Здание №2
-
 "use strict";
 
 function solveEquation(a, b, c) {
